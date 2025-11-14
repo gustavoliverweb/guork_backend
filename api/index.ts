fcd2bcf56388e820beff504db6b4197b938f5079
gustavoliverweb <gustavo@santiagodev.com>
2025-11-14 11:28:54 -0400

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,13 @@
+declare module "../dist/app"; // para TS7016
+
 import app from "../dist/app";
-import http from "http";
-import { VercelRequest, VercelResponse } from "@vercel/node";
+import http, { IncomingMessage, ServerResponse } from "http";
+
+type VercelRequest = IncomingMessage;
+type VercelResponse = ServerResponse;
 
 const server = http.createServer(app);
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-    server.emit("request", req, res);
+  server.emit("request", req, res);
 }
