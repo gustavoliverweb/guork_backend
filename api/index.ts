3002a7fd26372db0a31f950586d618abdd7c8554
gustavoliverweb <gustavo@santiagodev.com>
2025-11-14 14:08:06 -0400

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,24 +1,26 @@
 import serverless from "serverless-http";
-import app from "../src/app";
-import sequelize from "../src/config/database";
+import app from "../../src/app";
+import sequelize from "../../src/config/database";
 
+let isInitialized = false;
 let handler: any;
 
 const initializeDB = async () => {
+  if (isInitialized) return;
   try {
     console.log("Attempting database connection...");
     await sequelize.authenticate();
     console.log("✅ Database connected successfully");
   } catch (err) {
     console.error("❌ DB connection failed:", err);
-    throw err; // opcional
   }
+  isInitialized = true;
 };
 
-export default async (req: any, res: any) => {
+export default async function (req: any, res: any) {
   if (!handler) {
-    await initializeDB(); // <-- esperamos la conexión
+    await initializeDB(); // Esperamos la DB antes de inicializar serverless
     handler = serverless(app);
   }
   return handler(req, res);
-};
+}
